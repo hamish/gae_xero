89e5aa134cf50ba0013be3c4fd9f5f5f38a464ef
Hamish Currie <hamish@currie.com.au>
2014-02-25 08:58:00 +1000

    sync

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 gae_xero
 ========
 
-Playground for testing xero integration with google appen engine
+Playground for testing xero integration with google appenengine
+
+http://stackoverflow.com/questions/21868826/access-xero-from-google-app-engine
+
+http://hustoknow.blogspot.com.au/2012/01/using-pycrypto-instead-of-m2crypto-on.html
+
+https://github.com/freakboy3742/pyxero
---
+++ xerointegration.py
@@ -0,0 +1,57 @@
+import webapp2
+import jinja2
+import os
+from google.appengine.ext import ndb
+import logging
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class Contact(ndb.Model):
+    name=ndb.StringProperty()
+    email=ndb.StringProperty()
+    xero_guid = ndb.StringProperty()
+
+class InvoiceLine(ndb.Model):
+    description = ndb.StringProperty()
+    amount_cents = ndb.IntegerProperty()
+
+class Invoice(ndb.Model):
+    contact_key = ndb.KeyProperty()
+    line_items = ndb.LocalStructuredProperty(InvoiceLine, repeated=True)
+
+class MakeInvoice(webapp2.RequestHandler):
+    def get(self):
+        template_values={}
+        template = JINJA_ENVIRONMENT.get_template('make_invoice.html')
+        RSA_KEY_FILE="keys/rsa_key_file"
+        CONSUMER_KEY=""
+        
+        from xero import Xero
+        from xero.auth import PrivateCredentials
+        with open(RSA_KEY_FILE) as keyfile:
+            rsa_key = keyfile.read()
+        credentials = PrivateCredentials(CONSUMER_KEY, rsa_key)
+        xero = Xero(credentials)        
+        
+        self.response.write(template.render(template_values))        
+ 
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        
+        template_values={
+        'all_contacts' : Contact.query().fetch(),
+        'all_invoices' : Invoice.query().fetch(),
+        }
+        
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/make_invoice', MakeInvoice),
+], debug=True)
