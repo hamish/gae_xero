57ed136d3d1755446f881ac07fd76f5e9c5a5597
Hamish Currie <hamish@currie.com.au>
2014-02-25 20:14:09 +1000

    clean up the code

--- utils.py
+++ utils.py
@@ -1,16 +1,7 @@
 def get_contacts():
     from private import secret
-    RSA_KEY_FILE="private/privatekey.pem"
-    CONSUMER_KEY= secret.consumer_key
-    
-    rsa_key=''
-    with open(RSA_KEY_FILE) as keyfile:
-        rsa_key = keyfile.read()
-        
-        
     from xero import Xero
     from xero.auth import PrivateCredentials
-    
     credentials = PrivateCredentials(secret.consumer_key, secret.rsa_key_data)
     xero = Xero(credentials)
     
